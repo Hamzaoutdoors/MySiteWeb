c24deb3288edb37d4eee7e2934427dccda92ce2f
Hamza Outdoors <hamzaoutdoors@gmail.com>
2021-04-22 04:09:30 +0000

    Add ExplorePage

--- ExplorePage/script.js
+++ ExplorePage/script.js
@@ -3,11 +3,14 @@ let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
 timeline
+  .to(".me4", 10, { y: -200 })
+  .fromTo(".me4Tach", { y: -60, x: -30 }, { y: 0, x : - 150, duration: 10}, "-=10")
   .to(".me", 10, { y: -200 })
   .fromTo(".bg1", { y: -50 }, { y: 0, duration: 10 }, "-=10")
   .to(".content", 10, { top: "0%" }, "-=10")
   .fromTo(".content-images", { opacity: 0 }, { opacity: 1, duration: 3 })
   .fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 3 });
+  
 
 let scene = new ScrollMagic.Scene({
   triggerElement: "section",
